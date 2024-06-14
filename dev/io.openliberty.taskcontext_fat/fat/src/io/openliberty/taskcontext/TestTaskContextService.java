c745591222b90517299f66277c4d362b327b6dfc
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2024-06-14 14:17:36 +0100

    Simplify and rename test project elements
    Co-authored-by: Theo Gkoumas <169156855+TheoGkoumas@users.noreply.github.com>
    Co-authored-by: Martin Drozdz <46496604+martindrozdz@users.noreply.github.com>
    Co-authored-by: Neil Richards <7203556+ngmr@users.noreply.github.com>

---
+++ dev/io.openliberty.taskcontext_fat/fat/src/io/openliberty/taskcontext/TestTaskContextService.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.taskcontext;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.annotation.Server;
+import componenttest.annotation.TestServlet;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.FATServletClient;
+import test.servlet.TestTaskContextServiceServlet;
+
+@RunWith(FATRunner.class)
+public class TestTaskContextService extends FATServletClient {
+	public static final String APP_NAME = "test-servlet";
+    @Server("FATServer")
+    @TestServlet(servlet = TestTaskContextServiceServlet.class, contextRoot = APP_NAME)
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ShrinkHelper.defaultApp(server, APP_NAME, TestTaskContextServiceServlet.class.getPackage().getName());
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stopServer();
+    }
+}
