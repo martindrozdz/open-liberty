405f71a16bbbeea77e3db2b4f593ce7b4a79e678
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2024-06-18 14:41:17 +0100

    Make TestService a task interceptor
    Co-authored-by: Theo Gkoumas <169156855+TheoGkoumas@users.noreply.github.com>
    Co-authored-by: Martin Drozdz <46496604+martindrozdz@users.noreply.github.com>
    Co-authored-by: Neil Richards <7203556+ngmr@users.noreply.github.com>

--- dev/io.openliberty.taskcontext_fat/fat/src/test/bundle/TestService.java
+++ dev/io.openliberty.taskcontext_fat/fat/src/test/bundle/TestService.java
@@ -1,17 +1,44 @@
 package test.bundle;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
-@Component
-public class TestService {
-	private static final AtomicInteger activatedCount = new AtomicInteger();
-	private final int instance = activatedCount.incrementAndGet();
-	
-	public static int getActivatedCount() { return activatedCount.get(); }
+import com.ibm.wsspi.threading.ExecutorServiceTaskInterceptor;
 
-	@Activate
-	public void activate() { System.out.println("### Activate TestService#" + instance); }
+@Component
+public class TestService implements ExecutorServiceTaskInterceptor {
+    private static final AtomicInteger activatedCount = new AtomicInteger();
+    private final int instance = activatedCount.incrementAndGet();
+
+    public static int getActivatedCount() {
+        return activatedCount.get();
+    }
+
+    @Activate
+    public void activate() {
+        System.out.println("### Activate TestService#" + instance);
+    }
+
+    @Override
+    public Runnable wrap(Runnable r) {
+        new Throwable("wrapping runnable " + r).printStackTrace(System.out);
+        return () -> {
+            new Throwable("wrapped runnable invoked").printStackTrace(System.out);
+           r.run();
+        };
+    }
+
+    @Override
+    public <T> Callable<T> wrap(Callable<T> c) {
+        new Throwable("wrapping callable " + c).printStackTrace(System.out);
+        return () -> {
+            new Throwable("wrapped callable invoked").printStackTrace(System.out);
+            T t = c.call();
+            System.out.println("task returned " + t);
+            return t;
+        };
+    }
 }
